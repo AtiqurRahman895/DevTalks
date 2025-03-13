c7e5e9d94001a76656cf1e0550fa81c273d0c456
Majharulislam <majhuruislam535@gmail.com>
2025-03-13 23:28:52 +0600

    Complete project

--- src/components/BaseComponents/Base.jsx
+++ src/components/BaseComponents/Base.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Footer from './Footer';
+import Navbar from './Navbar';
 
 const Base = () => {
     return (
         <>
+        <Navbar></Navbar>
         <Outlet />
         <Footer></Footer>
         </>
