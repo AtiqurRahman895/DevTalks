6b6eb3b98e546752c9d7cbec6ea61db91230a4f7
myFirstgitrepo <tanvirmahin607@gmail.com>
2025-03-20 17:45:25 +0600

    find the user for inbox by using find method

--- src/components/TextMessage/UserInbox.jsx
+++ src/components/TextMessage/UserInbox.jsx
@@ -1,11 +1,72 @@
 import React from 'react'
 import { useParams } from 'react-router'
 
-const UserInbox = () => {
-    const {user} = useParams();
+const UserInbox = () => {  
+    const {user: currentUser} = useParams(); 
+
+    const users = [
+        {
+          id: 1,
+          name: "Jhon Wick",
+          email: "jhon.wick@email.com",
+          photo:
+            "https://i.pinimg.com/736x/92/6c/8e/926c8e161cde3909dcd0f54fa1ff9483.jpg",
+        },
+        {
+          id: 2,
+          name: "Alice Johnson",
+          email: "alice.johnson@email.com",
+          photo: "https://randomuser.me/api/portraits/women/44.jpg",
+        },
+        {
+          id: 3,
+          name: "Michael Brown",
+          email: "michael.brown@email.com",
+          photo: "https://randomuser.me/api/portraits/men/32.jpg",
+        },
+        {
+          id: 4,
+          name: "Emily Davis",
+          email: "emily.davis@email.com",
+          photo: "https://randomuser.me/api/portraits/women/68.jpg",
+        },
+        {
+          id: 5,
+          name: "Robert Wilson",
+          email: "robert.wilson@email.com",
+          photo: "https://randomuser.me/api/portraits/men/57.jpg",
+        },
+        {
+          id: 5,
+          name: "Robert Wilson",
+          email: "robert.wilson@email.com",
+          photo: "https://randomuser.me/api/portraits/men/57.jpg",
+        },
+        {
+          id: 5,
+          name: "Robert Wilson",
+          email: "robert.wilson@email.com",
+          photo: "https://randomuser.me/api/portraits/men/57.jpg",
+        },
+        {
+          id: 5,
+          name: "Robert Wilson",
+          email: "robert.wilson@email.com",
+          photo: "https://randomuser.me/api/portraits/men/57.jpg",
+        },
+        {
+          id: 5,
+          name: "Robert Wilson",
+          email: "robert.wilson@email.com",
+          photo: "https://randomuser.me/api/portraits/men/57.jpg",
+        },
+      ];
+
+      const inboxUser = users.find(user => user.name === currentUser)
+      console.log(inboxUser)
+
   return (
     <div>
-        {user}
     </div>
   )
 }
