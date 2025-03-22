f3d9a97614ba52f3cd4d08e6d7bffb77b6e90a78
Majharulislam <majhuruislam535@gmail.com>
2025-03-22 16:43:29 +0600

    complete card section

--- src/components/Dashboard/Admin_Panel/Admin_Panel.jsx
+++ src/components/Dashboard/Admin_Panel/Admin_Panel.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
-
+import { FcTimeline } from "react-icons/fc";
 const Admin_Panel = () => {
     return (
         <div>
-            <h1>this is admin page</h1>
+            {/* card section  */}
+
+            <div className="grid lg:grid-cols-4 gap-5 md:grid-cols-4 sm:grid-cols-1">
+
+                <div className='bg-custom-primary rounded-lg p-6 flex justify-between items-center'>
+                    <div>
+                        <p>Total Traffic</p>
+                        <h4>34,500</h4>
+                        <p> <span className='text-green-300'>3.45% </span> science last Month</p>
+                    </div>
+                    <div>
+                        <FcTimeline className='bg-white text-6xl p-3 rounded-full' />
+                    </div>
+                </div>
+
+                <div className='bg-custom-primary rounded-lg p-6 flex justify-between items-center'>
+                    <div>
+                        <p>Total Traffic</p>
+                        <h4>34,500</h4>
+                        <p> <span className='text-green-300'>3.45% </span> science last Month</p>
+                    </div>
+                    <div>
+                        <FcTimeline className='bg-white text-6xl p-3 rounded-full' />
+                    </div>
+                </div>
+
+                <div className='bg-custom-primary rounded-lg p-6 flex justify-between items-center'>
+                    <div>
+                        <p>Total Traffic</p>
+                        <h4>34,500</h4>
+                        <p> <span className='text-green-300'>3.45% </span> science last Month</p>
+                    </div>
+                    <div>
+                        <FcTimeline className='bg-white text-6xl p-3 rounded-full' />
+                    </div>
+                </div>
+
+                <div className='bg-custom-primary rounded-lg p-6 flex justify-between items-center'>
+                    <div>
+                        <p>Total Traffic</p>
+                        <h4>34,500</h4>
+                        <p> <span className='text-green-300'>3.45% </span> science last Month</p>
+                    </div>
+                    <div>
+                        <FcTimeline className='bg-white text-6xl p-3 rounded-full' />
+                    </div>
+                </div>
+
+
+                 
+            </div>
+
+
         </div>
     );
 };
