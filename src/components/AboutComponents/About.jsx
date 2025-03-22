6513b03fe59187d28816e72ed5b5b2389ca90058
myFirstgitrepo <tanvirmahin607@gmail.com>
2025-03-22 17:48:50 +0600

    added a about us highlight section title and title image

--- src/components/AboutComponents/About.jsx
+++ src/components/AboutComponents/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import SectionBanner from '../CommonComponents/SectionBanner'
+import AboutUsHighlight from './AboutUsHighlight/AboutUsHighlight'
 
 export default function About() {
   return (
-    <div>
+    <div className='container'>
       <SectionBanner title={"About Us"} />
       
       <AboutUsHighlight />
--- src/components/AboutComponents/AboutUsHighlight/AboutUsHighlight.jsx
+++ src/components/AboutComponents/AboutUsHighlight/AboutUsHighlight.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function AboutUsHighlight() {
   return (
-    <div>
-      
+    <div className="container px-6">
+      {/* title and title photo */}
+
+      <div className="w-[50%]">
+        <h3 className="font-bold border-l-8 border-custom-primary pl-4 mb-6">
+          Connecting Developers, Sharing Knowledge, and Building a Stronger
+          <strong className="text-custom-primary text-4xl ml-2">Tech Community</strong>
+        </h3>
+        <img src="/AboutUs1.jpg" alt="" className="w-full h-72 object-cover rounded-xl" />
+      </div>
+
+      {/* description and users, questions */}
+      <div>
+        {/* two image side by side */}
+         <div className="flex items-center gap-6">
+            <img src="/AboutUs2.jpg" alt="" />
+            <img src="/AboutUs3.jpg" alt="" />
+         </div>
+      </div>
     </div>
-  )
+  );
 }
