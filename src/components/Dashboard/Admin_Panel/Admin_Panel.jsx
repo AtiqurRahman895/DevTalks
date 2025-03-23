54e85e17f258a2c0d2e1d08ee3fd514ddf206643
Majharulislam <majhuruislam535@gmail.com>
2025-03-23 16:16:07 +0600

    add barchats and  linechart

--- src/components/Dashboard/Admin_Panel/Admin_Panel.jsx
+++ src/components/Dashboard/Admin_Panel/Admin_Panel.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { FcTimeline } from "react-icons/fc";
+import { FcConferenceCall } from "react-icons/fc";
+import { FcDocument } from "react-icons/fc";
+import { FcComboChart } from "react-icons/fc";
+import LineCharts from './LineChart';
+import CustomBarChart from './BarCharts';
+
 const Admin_Panel = () => {
     return (
         <div>
@@ -18,42 +24,71 @@ const Admin_Panel = () => {
                     </div>
                 </div>
 
+
+
+
+
                 <div className='bg-custom-primary rounded-lg p-6 flex justify-between items-center'>
                     <div>
-                        <p>Total Traffic</p>
-                        <h4>34,500</h4>
+                        <p>Total User</p>
+                        <h4>4,500</h4>
                         <p> <span className='text-green-300'>3.45% </span> science last Month</p>
                     </div>
                     <div>
-                        <FcTimeline className='bg-white text-6xl p-3 rounded-full' />
+                        <FcConferenceCall className='bg-white text-6xl p-3 rounded-full' />
+                    </div>
+                </div>
+
+
+
+
+                <div className='bg-custom-primary rounded-lg p-6 flex justify-between items-center'>
+                    <div>
+                        <p>Total Post</p>
+                        <h4>2,570</h4>
+                        <p> <span className='text-green-300'>3.45% </span> science last Month</p>
+                    </div>
+                    <div>
+                        <FcDocument className='bg-white text-6xl p-3 rounded-full' />
                     </div>
                 </div>
 
                 <div className='bg-custom-primary rounded-lg p-6 flex justify-between items-center'>
                     <div>
-                        <p>Total Traffic</p>
-                        <h4>34,500</h4>
+                        <p>Performance</p>
+                        <h4>67%</h4>
                         <p> <span className='text-green-300'>3.45% </span> science last Month</p>
                     </div>
                     <div>
-                        <FcTimeline className='bg-white text-6xl p-3 rounded-full' />
-                    </div>
-                </div>
-
-                <div className='bg-custom-primary rounded-lg p-6 flex justify-between items-center'>
-                    <div>
-                        <p>Total Traffic</p>
-                        <h4>34,500</h4>
-                        <p> <span className='text-green-300'>3.45% </span> science last Month</p>
-                    </div>
-                    <div>
-                        <FcTimeline className='bg-white text-6xl p-3 rounded-full' />
+                        <FcComboChart className='bg-white text-6xl p-3 rounded-full' />
                     </div>
                 </div>
 
 
-                 
+
             </div>
+
+
+            {/* line charts  */}
+
+
+            <div className="flex flex-col md:flex-row my-7 justify-between items-center">
+                <div className="w-full md:w-4/6 md:mr-4">
+                    
+                    <LineCharts></LineCharts>
+                </div>
+                <div className="w-full md:w-2/6">
+                    <CustomBarChart></CustomBarChart>
+                </div>
+            </div>
+
+            {/* table  card  */}
+
+            <div className='grid grid-cols-3 '>
+
+            </div>
+
+
 
 
         </div>
