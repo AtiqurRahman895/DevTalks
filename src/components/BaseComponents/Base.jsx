4e81ca348989cac9552bf80e483f1093e221ce28
Majharulislam <majhurulislam535@gmail.com>
2025-04-04 23:29:06 +0600

    add eid wishlist

--- src/components/BaseComponents/Base.jsx
+++ src/components/BaseComponents/Base.jsx
@@ -4,12 +4,14 @@ import Footer from './Footer';
 import Navbar from './Navbar';
 import NavMenuSidebar from './NavMenuSidebar';
 import useGetUserRole from '../../Hooks/useGetUserRole'
+import Eid_wish_List from './Eid_wish_List';
 
 const Base = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     useGetUserRole()
     return (
         <>
+        <Eid_wish_List></Eid_wish_List>
         <NavMenuSidebar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Outlet />
