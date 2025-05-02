5cb4dc7fcfe32bcd5d55efb834c20ea5288293d5
Majharulislam <majhurulislam535@gmail.com>
2025-05-02 19:46:22 +0600

    user add chat list

--- src/components/Chat_Component/Chat.jsx
+++ src/components/Chat_Component/Chat.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import io from 'socket.io-client';
 import chat_bg from '../../assets/chat_bg.jpg';
+import { AuthContext } from "../../Provider/AuthProvider";
 
 function Chat() {
+  const { user } = useContext(AuthContext)
   const [username, setUsername] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
@@ -46,12 +48,14 @@ function Chat() {
     fetchData();
   }, []);
 
-  const handleJoin = () => {
-    if (username.trim() && !hasJoined) {
-      socketRef.current.emit('join', username);
+   
+
+  useEffect(() => {
+    if (user.email) {
+      socketRef.current.emit('join',user);
       setHasJoined(true);
     }
-  };
+  }, [user])
 
   const handleSendMessage = (e) => {
     e.preventDefault();
@@ -70,31 +74,16 @@ function Chat() {
     setSelectedUser(user.name);
   };
 
-   
+
+  console.log(user?.email);
+
 
   return (
     <div className="min-h-screen bg-black flex items-center justify-center">
-      {!hasJoined ? (
+      {!user?.email ? (
         <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-          <h1 className="text-2xl font-bold mb-4 text-center">Join Private Chat</h1>
           {loading && <p className="text-center text-gray-500">Loading users...</p>}
           {error && <p className="text-center text-red-500">{error}</p>}
-          <div className="space-y-4">
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Enter your username"
-              className="w-full p-2 border text-black rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <button
-              onClick={handleJoin}
-              type="submit"
-              className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition"
-            >
-              Join
-            </button>
-          </div>
         </div>
       ) : (
         <div className="flex w-full h-screen">
@@ -120,13 +109,13 @@ function Chat() {
           <div className="flex-1 flex flex-col p-4">
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-xl font-semibold">
-                  <img src="" alt="" />
+                <img src="" alt="" />
                 {selectedUser ? `Chatting with @${selectedUser}` : 'Select a user to start chatting'}
               </h2>
               <span className="text-sm text-gray-500">{isConnected ? '🟢 Connected' : '🔴 Disconnected'}</span>
             </div>
 
-            <div className="flex-1 overflow-y-auto p-4 bg-gray-900 rounded border mb-4 bg-blend-overlay" style={{backgroundImage:`url(${chat_bg})`}}>
+            <div className="flex-1 overflow-y-auto p-4 bg-gray-900 rounded border mb-4 bg-blend-overlay" style={{ backgroundImage: `url(${chat_bg})` }}>
               {selectedUser ? (
                 messages
                   .filter(
@@ -143,7 +132,7 @@ function Chat() {
                         className={`inline-block p-2 rounded-full px-4 ${msg.from === username
                           ? 'bg-blue-500 text-white'
                           : 'bg-gray-200 text-gray-800'
-                        }`}
+                          }`}
                       >
                         <strong>{msg.from}:</strong> {msg.message}
                       </span>
